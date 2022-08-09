b659be9ebe6ef380be824d588f7ab8eeadeb40da
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-09 22:38:42 +0900

    #11 Connection with database 1

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Server is listening at ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server is listening on ${PORT}`);
+    });
+})
