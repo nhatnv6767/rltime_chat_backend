e41a6e60ad5e472e6cfa28b5734e08078ba73731
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-09 22:52:23 +0900

    #12 Authentication routes 2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,3 +4,7 @@ const router = express.Router();
 router.post('/register', (req, res) => {
     res.send("register route")
 })
+
+router.post("/login", (req, res) => {
+    res.send("login route")
+})
