9814666b77bd8ebc9995858a9426b5d138018403
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 01:47:59 +0900

    #25 Middleware to check if token is valid and not expired 3

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 
+
 const config = process.env;
 
 // next: execute the next func if we'll just complete successfully our logic and will execute the next func
@@ -13,7 +14,12 @@ const verifyToken = (req, res, next) => {
         return res.status(403).send("A token is required for authentication")
     }
     try {
-        token = token.replace()
+        // remove Bearer keyword and one space (s)
+        token = token.replace(/^Bearer\s+/, "");
+        const decoded = jwt.verify(
+            token, config.TOKEN_KEY
+        )
+        req.user = decoded;
     } catch (e) {
         return res.status(401).send("Invalid token")
     }
