22116323ae40965a1178a187a350a33e7699a9fc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 01:52:15 +0900

    #25 Middleware to check if token is valid and not expired 5

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require("../controllers/auth/authControllers")
 const Joi = require('joi')
 const validator = require("express-joi-validation").createValidator({});
+const auth = require("../middleware/auth")
 
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
@@ -27,5 +28,8 @@ router.post(
 )
 
 // testing middleware
+router.get('/test', auth, (req, res) => {
+    res.send("Request passed")
+})
 
 module.exports = router;
