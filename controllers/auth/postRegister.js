d27ebc410218a53634c3fa2fc6efd69c91c2fdc3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 01:00:22 +0900

    #19 Creating register functionality 3

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,12 +1,18 @@
 const User = require("../../models/user")
+const bcrypt = require("bcryptjs")
 const postRegister = async (req, res) => {
     try {
         const {username, mail, password} = req.body;
 
         // check if user exists
         const userExists = await User.exists({
-            mail: mail,
+            mail
         })
+        if(userExists){
+            return res.status(409).send("E-mail already in use")
+        }
+        // encrypt password
+        const encryptedPassword = await bcrypt.hash(password, 10);
     } catch (e) {
         return ré.status(500).send("Error occurred. Please try again.");
     }
