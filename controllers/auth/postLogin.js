b92e090dfc007a204f07d3f81427b5c81671f46b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 01:17:08 +0900

    #21 Creating login functionality 2

--- controllers/auth/postLogin.js
+++ controllers/auth/postLogin.js
@@ -3,7 +3,15 @@ const bcrypt = require("bcryptjs")
 
 const postLogin = async (req, res) => {
     try {
-        res.send("login route");
+        const {mail, password} = req.body;
+        const user = await User.findOne({
+            mail: mail.toLowerCase(),
+        })
+        if(user && (await bcrypt.compare(password, user.password))) {
+            // send new token
+        }
+
+        return res.status(400).send("Invalid credentials. Please try again.")
     } catch (e) {
         return res.status(500).send("Error occurred. Please try again.");
     }
