5b9bda0eecde798621dd3ae695f52e15c7067ad3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-10 21:22:30 +0900

    #15 Validation with joi 1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const authControllers = require("../controllers/auth/authControllers")
+const Joi = require('joi')
+const validator = require("express-joi-validation").createValidator({});
+
+
 
 router.post('/register', authControllers.controllers.postRegister)
-
 router.post("/login", authControllers.controllers.postLogin)
 
 module.exports = router;
