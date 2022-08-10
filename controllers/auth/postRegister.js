70a99c495abc2c1e95a6a69447d070d02dd43796
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 00:56:55 +0900

    #19 Creating register functionality 2

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,6 +1,12 @@
-
+const User = require("../../models/user")
 const postRegister = async (req, res) => {
     try {
+        const {username, mail, password} = req.body;
+
+        // check if user exists
+        const userExists = await User.exists({
+            mail: mail,
+        })
     } catch (e) {
         return ré.status(500).send("Error occurred. Please try again.");
     }
