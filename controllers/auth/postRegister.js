ad3d5403f55e34ce125c18194dc81f788edca42c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-11 01:03:04 +0900

    #19 Creating register functionality 4

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -6,13 +6,22 @@ const postRegister = async (req, res) => {
 
         // check if user exists
         const userExists = await User.exists({
-            mail
+            mail: mail.toLowerCase()
         })
         if(userExists){
             return res.status(409).send("E-mail already in use")
         }
         // encrypt password
         const encryptedPassword = await bcrypt.hash(password, 10);
+        // create user document and save in database
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword,
+        })
+
+        // create JWT token
+
     } catch (e) {
         return ré.status(500).send("Error occurred. Please try again.");
     }
