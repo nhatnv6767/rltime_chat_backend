223c4c5cbfe7783a789996b8df3d8eb6a63bccab
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 00:49:30 +0900

    #71 Validating JWT Tokens at Server Side 3

--- socketServer.js
+++ socketServer.js
@@ -1,8 +1,15 @@
+const authSocket = require('./middleware/authSocket');
+
 const registerSocketServer = (server) => {
   const io = require('socket.io')(server, {
     cors: {
       origin: '*', methods: ['GET', 'POST'],
     },
+  });
+
+  io.use((socket, next) => {
+    authSocket(socket, next);
+
   });
 
   io.on('connection', (socket) => {
