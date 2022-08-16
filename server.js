36f80a31f5f15d66a584dc3549f8f7918fc404c6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 23:57:30 +0900

    #68 Connecting SocketIO to Server 5

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-const express = require('express')
-const http = require('http')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require("dotenv").config();
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const authRoutes = require('./routes/authRoutes')
+const socketServer = require('./socketServer');
+const authRoutes = require('./routes/authRoutes');
 
-const PORT = process.env.PORT || process.env.API_PORT
+const PORT = process.env.PORT || process.env.API_PORT;
 
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
 
 const server = http.createServer(app);
+socketServer.registerSocketServer(server);
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        server.listen(PORT, () => {
-            console.log(`Server is listening on ${PORT}`);
-        });
-    })
-    .catch(err =>{
-        console.log("Database connection failed. Server not started.");
-        console.error(err)
-    })
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+  });
+}).catch(err => {
+  console.log('Database connection failed. Server not started.');
+  console.error(err);
+});
