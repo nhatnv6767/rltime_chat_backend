b54f6be83ac6b8da56326aa39d035d8e418f4786
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-16 23:49:19 +0900

    #68 Connecting SocketIO to Server 3

--- socketServer.js
+++ socketServer.js
@@ -1,3 +1,12 @@
 const registerSocketServer = (server) => {
-  const io = require('socket.io')(server, {});
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: '*', methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('user connected');
+    console.log(socket.id);
+  });
 };
