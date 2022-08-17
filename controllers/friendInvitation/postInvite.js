8bc13ab2136b34420976cbf590488b780845cbb4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 20:31:49 +0900

    #80 Custom Friend invitation validation 2

--- controllers/friendInvitation/postInvite.js
+++ controllers/friendInvitation/postInvite.js
@@ -3,6 +3,12 @@ const postInvite = async (req, res) => {
 
     const {userId, mail} = req.user;
 
+    // check if friend that we would like to invite is not user
+
+    if (mail.toLowerCase() === targetMailAddress.toLowerCase()) {
+        return res.status(409).send("Sorry. You cannot become friend with yourself.");
+    }
+
     return res.send("Controller is working");
 }
 
