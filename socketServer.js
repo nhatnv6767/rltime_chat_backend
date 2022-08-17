251ca8e81f510bc2a00bb88d9630e1a5eefb6a03
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 17:54:07 +0900

    #74 Creating Disconnect Handler 5

--- socketServer.js
+++ socketServer.js
@@ -1,5 +1,6 @@
 const authSocket = require('./middleware/authSocket');
 const newConnectionHandler = require("./socketHandlers/newConnectionHandler");
+const disconnectHandler = require("./socketHandlers/disconnectHandler");
 
 const registerSocketServer = (server) => {
     const io = require('socket.io')(server, {
