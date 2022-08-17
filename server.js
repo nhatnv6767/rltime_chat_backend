b649986873ce5c2b7893eb7d1b5e6f94082746e1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 18:34:26 +0900

    #78 Friends invitations 1

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const socketServer = require('./socketServer');
 const authRoutes = require('./routes/authRoutes');
+const friendInvitationRoutes = require('./routes/friendInvitationRoutes');
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
@@ -14,15 +15,16 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
+app.use("/api/friend-invitation", friendInvitationRoutes)
 
 const server = http.createServer(app);
 socketServer.registerSocketServer(server);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  server.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
-  });
+    server.listen(PORT, () => {
+        console.log(`Server is listening on ${PORT}`);
+    });
 }).catch(err => {
-  console.log('Database connection failed. Server not started.');
-  console.error(err);
+    console.log('Database connection failed. Server not started.');
+    console.error(err);
 });
