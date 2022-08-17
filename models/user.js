c82eb827bea9804727f8542a2692f6b3bf4b5b4f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 20:45:20 +0900

    #82 Custom Validation Related with Friend invitations 3

--- models/user.js
+++ models/user.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
+const {Schema} = require("mongoose");
 
 
 const userSchema = new mongoose.Schema({
     mail: {type: String, unique: true},
     username: {type: String},
     password: {type: String},
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ]
 })
 
 module.exports = mongoose.model("User", userSchema)
