8cdaab1ecf67ce2a45e39f8b2093cab4fc106416
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 00:27:43 +0900

    #86 Preparing at Server pending friend invitations 2

--- socketHandlers/updates/friends.js
+++ socketHandlers/updates/friends.js
@@ -3,4 +3,11 @@ const FriendInvitation = require("../../models/friendInvitation")
 const serverStore = require("../../serverStore")
 
 const updateFriendsPendingInvitation = async (userId) => {
+    try {
+        const pendingInvitation = await FriendInvitation.find({
+            receiverId: userId
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
