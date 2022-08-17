3105420490d70f38dc6c417201f30477784bc978
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 00:24:46 +0900

    #86 Preparing at Server pending friend invitations 1

---
+++ socketHandlers/updates/friends.js
@@ -0,0 +1,6 @@
+const User = require("../../models/user")
+const FriendInvitation = require("../../models/friendInvitation")
+const serverStore = require("../../serverStore")
+
+const updateFriendsPendingInvitation = async (userId) => {
+}
