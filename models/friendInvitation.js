915643552e3365d641e02559e58642a59fe93fb3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 19:03:41 +0900

    #79 Creating Friend invitation Model 2

--- models/friendInvitation.js
+++ models/friendInvitation.js
@@ -6,6 +6,13 @@ const Schema = mongoose.Schema;
 const friendInvitationSchema = new Schema({
     senderId: {
         type: Schema.Types.ObjectId,
+        /* Telling mongoose that the `senderId` is a reference to the `User` model. */
+        ref: "User"
+    },
+    receiverId: {
+        type: Schema.Types.ObjectId,
         ref: "User"
     }
 })
+
+module.exports = mongoose.model("FriendInvitation", friendInvitationSchema)
