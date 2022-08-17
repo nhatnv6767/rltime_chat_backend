b48eacddb07e5c3f97a1e6f5c3a97a8db49bb728
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 16:20:49 +0900

    #72 Server Store - Saving information about connected users 6

--- serverStore.js
+++ serverStore.js
@@ -1,11 +1,13 @@
 const connectedUsers = new Map();
 
 const addNewConnectedUser = ({socketId, userId}) => {
-  /* Adding a new key-value pair to the Map. */
-  connectedUsers.set(socketId, {userId});
+    /* Adding a new key-value pair to the Map. */
+    connectedUsers.set(socketId, {userId});
+    console.log("New connected users")
+    console.log(connectedUsers)
 };
 
 module.exports = {
-  addNewConnectedUser,
-  
+    addNewConnectedUser,
+
 };
