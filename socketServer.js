c7f9abb3ca0a89f75ff01326d7377c8c76ff88ba
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-17 16:18:56 +0900

    #72 Server Store - Saving information about connected users 5

--- socketServer.js
+++ socketServer.js
@@ -1,24 +1,25 @@
 const authSocket = require('./middleware/authSocket');
+const newConnectionHandler = require("./socketHandlers/newConnectionHandler");
 
 const registerSocketServer = (server) => {
-  const io = require('socket.io')(server, {
-    cors: {
-      origin: '*', methods: ['GET', 'POST'],
-    },
-  });
+    const io = require('socket.io')(server, {
+        cors: {
+            origin: '*', methods: ['GET', 'POST'],
+        },
+    });
 
-  io.use((socket, next) => {
-    authSocket(socket, next);
+    io.use((socket, next) => {
+        authSocket(socket, next);
 
-  });
+    });
 
-  io.on('connection', (socket) => {
-    console.log('user connected');
-    console.log(socket.id);
-    newConnectionHandler(socket, io);
-  });
+    io.on('connection', async (socket) => {
+        console.log('user connected');
+        console.log(socket.id);
+        await newConnectionHandler(socket, io);
+    });
 };
 
 module.exports = {
-  registerSocketServer,
+    registerSocketServer,
 };
