5376c6052d8c81bd5d644288dc48a2749624b5b7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 01:00:17 +0900

    #117 Handler for direct message event 6

--- socketHandlers/directMessageHandler.js
+++ socketHandlers/directMessageHandler.js
@@ -1,8 +1,19 @@
+const Message = require('../models/message')
+
 const directMessageHandler = async (socket, data) => {
     try {
         const {userId} = socket.user;
         const {receiverUserId, content} = data;
-        
+
+        // create new message
+        const message = await Message.create({
+            content: content,
+            authorId: userId,
+            date: new Date(),
+            type: "DIRECT",
+        })
+
+        //find if conversation exist with this two users - if not create new
     } catch (err) {
         console.log(err);
     }
