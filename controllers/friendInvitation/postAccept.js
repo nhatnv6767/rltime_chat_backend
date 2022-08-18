16e377a0c2c729dd95bd2d5e971b2baaa7d63603
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 16:07:38 +0900

    #98 Creating logic of accepting invitation 3

--- controllers/friendInvitation/postAccept.js
+++ controllers/friendInvitation/postAccept.js
@@ -1,9 +1,22 @@
 const FriendInvitation = require("../../models/friendInvitation")
+const User = require("../../models/user")
 
 const postAccept = async (req, res) => {
     try {
         const {id} = req.body;
-        const invitation = await FriendInvitation.findById(id)
+        const invitation = await FriendInvitation.findById(id);
+
+        if (!invitation) {
+            return res.status(401).send("Error occurred. Please try again.");
+        }
+
+        const {senderId, receiverId} = invitation;
+        // add friends to both users
+        const senderUser = await User.findById(senderId);
+        const receiverUser = await User.findById(receiverId);
+        senderUser.friends = [...senderUser.friends, receiverId]
+        receiverUser.friends = [...receiverUser.friends, senderId]
+        
     } catch (err) {
         console.log(err);
         return res.status(500).send("Something went wrong please try again");
