e8f223b52117b1eb2870b77b6658f9076df98215
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 17:46:34 +0900

    #101 Real Friends Updates 4

--- socketHandlers/updates/friends.js
+++ socketHandlers/updates/friends.js
@@ -25,32 +25,34 @@ const updateFriendsPendingInvitations = async (userId) => {
 
 const updateFriends = async (userId) => {
     try {
-        const user = await User.findById(userId, {_id: 1, friends: 1}).populate(
-            "friends",
-            "_id username mail"
-        )
+        // find active connections of specific id (online users)
+        const receiverList = serverStore.getActiveConnections(userId)
+        if (receiverList.length > 0) {
+            const user = await User.findById(userId, {_id: 1, friends: 1}).populate(
+                "friends",
+                "_id username mail"
+            )
 
-        if (user) {
-            const friendsList = user.friends.map(f => {
-                return {
-                    id: f._id,
-                    mail: f.mail,
-                    username: f.username,
-                }
-            })
-
-            // find active connections of specific id (online users)
-            const receiverList = serverStore.getActiveConnections(userId)
-
-            // get io server instance
-            const io = serverStore.getSocketServerInstance();
-
-            receiverList.forEach(receiverSocketId => {
-                io.to(receiverSocketId).emit("friends-list", {
-                    friends: friendsList ? friendsList : [],
+            if (user) {
+                const friendsList = user.friends.map(f => {
+                    return {
+                        id: f._id,
+                        mail: f.mail,
+                        username: f.username,
+                    }
                 })
-            })
+
+                // get io server instance
+                const io = serverStore.getSocketServerInstance();
+
+                receiverList.forEach(receiverSocketId => {
+                    io.to(receiverSocketId).emit("friends-list", {
+                        friends: friendsList ? friendsList : [],
+                    })
+                })
+            }
         }
+
     } catch (err) {
         console.log(err);
     }
