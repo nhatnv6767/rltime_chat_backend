cf987663b75e6bf8fad4d5af29db71fadd34deed
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 10:58:38 +0900

    #92 Initial update of pending invitations 1

--- socketHandlers/newConnectionHandler.js
+++ socketHandlers/newConnectionHandler.js
@@ -1,9 +1,13 @@
 const serverStore = require("../serverStore");
+const friendsUpdates = require("./updates/friends");
 const newConnectionHandler = async (socket, io) => {
     const userDetails = socket.user
     serverStore.addNewConnectedUser({
         socketId: socket.id,
         userId: userDetails.userId,
     })
+
+    // update pending friends invitations list
+    friendsUpdates.updateFriendsPendingInvitation(userDetails.userId);
 }
 module.exports = newConnectionHandler
