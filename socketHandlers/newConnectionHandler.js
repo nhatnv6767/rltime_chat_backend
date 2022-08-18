8dcfa42d86f3aed86b110108f4a9fac09627d4f8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 17:49:18 +0900

    #101 Real Friends Updates 5

--- socketHandlers/newConnectionHandler.js
+++ socketHandlers/newConnectionHandler.js
@@ -9,5 +9,8 @@ const newConnectionHandler = async (socket, io) => {
 
     // update pending friends invitations list
     await friendsUpdate.updateFriendsPendingInvitations(userDetails.userId);
+
+    // update friends list
+    await friendsUpdate.updateFriends(userDetails.userId)
 }
 module.exports = newConnectionHandler
