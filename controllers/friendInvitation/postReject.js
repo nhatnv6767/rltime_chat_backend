646b26613c6d13168640c4c4a1764bde400b54e3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-18 15:53:24 +0900

    #96 Creating logic of rejecting invitation 2

--- controllers/friendInvitation/postReject.js
+++ controllers/friendInvitation/postReject.js
@@ -1,5 +1,20 @@
+const FriendInvitation = require("../../models/friendInvitation")
+const friendsUpdates = require("../../socketHandlers/updates/friends");
 const postReject = async (req, res) => {
     try {
+        const {id} = req.body;
+        const {userId} = req.user;
+
+        // remove that invitation from friend invitations collection
+        const invitationExists = await FriendInvitation.exists({
+            _id: id
+        })
+        if (invitationExists) {
+            await FriendInvitation.findByIdAndUpdate(id)
+        }
+
+        // update pending invitations
+        await friendsUpdates.updateFriendsPendingInvitations(userId)
     } catch (err) {
         console.log(err);
         return res.status(500).send("Something went wrong please try again");
