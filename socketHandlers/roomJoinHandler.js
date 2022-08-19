4c18e9720e5efe7006960dca1be6c836af717d58
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 02:13:33 +0900

    #150 Joining active room 4

--- socketHandlers/roomJoinHandler.js
+++ socketHandlers/roomJoinHandler.js
@@ -1,1 +1,10 @@
 const serverStore = require("../serverStore");
+const roomsUpdate = require("./updates/rooms")
+
+const roomJoinHandler = (socket, data) => {
+    const {roomId} = data;
+
+    const participantDetails = {}
+}
+
+module.exports = roomJoinHandler
