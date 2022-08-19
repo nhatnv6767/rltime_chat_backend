9b8091ca699983b563ebfc3f48a30cf70bdd4728
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 00:11:43 +0900

    #144 Creating Room 4

--- socketServer.js
+++ socketServer.js
@@ -4,6 +4,7 @@ const disconnectHandler = require("./socketHandlers/disconnectHandler");
 const serverStore = require("./serverStore");
 const directMessageHandler = require("./socketHandlers/directMessageHandler")
 const directChatHistoryHandler = require("./socketHandlers/directChatHistoryHandler")
+const roomCreateHandler = require("./socketHandlers/roomCreateHandler")
 
 const registerSocketServer = (server) => {
     const io = require('socket.io')(server, {
