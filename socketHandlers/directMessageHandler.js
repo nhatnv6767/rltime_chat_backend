6113da8464fba28db94af740ef6a76fc3d4d3d1c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 19:32:18 +0900

    #126 Fixing bug with update of first message 1

--- socketHandlers/directMessageHandler.js
+++ socketHandlers/directMessageHandler.js
@@ -37,7 +37,7 @@ const directMessageHandler = async (socket, data) => {
             })
 
             // perform and update to sender and receiver if is online
-            await chatUpdates.updateChatHistory(conversation._id.toString())
+            await chatUpdates.updateChatHistory(newConversation._id.toString())
         }
     } catch (err) {
         console.log(err);
