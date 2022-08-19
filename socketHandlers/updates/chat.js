4641cefefe2b34bccec7af8579830c5a44e8d6c1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 13:32:53 +0900

    #119 Realtime chat updates 4

--- socketHandlers/updates/chat.js
+++ socketHandlers/updates/chat.js
@@ -1,7 +1,7 @@
 const Conversation = require('../models/conversation')
 const serverStore = require("../../serverStore")
 
-const updateChatHistory = async (conversationId) => {
+const updateChatHistory = async (conversationId, toSpecifiedSocketId = null) => {
     const conversation = await Conversation.findById(conversationId).populate({
         path: "messages",
         model: "Model",
@@ -15,5 +15,22 @@ const updateChatHistory = async (conversationId) => {
     if (conversation) {
         const io = serverStore.getSocketServerInstance();
 
+        if (toSpecifiedSocketId) {
+            // initial update of chat history
+        }
+
+        // check if users of this conversation are online
+        // if yes emit to them update of messages
+
+        conversation.participants.forEach(userId => {
+            const activeConnections = serverStore.getActiveConnections(userId.toString())
+
+            activeConnections.forEach(socketId => {
+                io.to(socketId).emit("direct-chat-history", {
+                    messages: conversation.messages,
+                    participants: conversation.participants,
+                })
+            })
+        })
     }
 }
