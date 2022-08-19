344bb73798affc5c037e329c0b7e3d77be9b4a24
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-19 13:23:55 +0900

    #119 Realtime chat updates 2

---
+++ socketHandlers/updates/chat.js
@@ -0,0 +1,6 @@
+const Conversation = require('../models/conversation')
+const serverStore = require("../../serverStore")
+
+const updateChatHistory = async (conversationId, toSpecificSocketId = null) => {
+    
+}
