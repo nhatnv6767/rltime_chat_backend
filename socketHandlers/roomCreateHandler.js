cb76e080094dd1669e91b3e83706f118b15912a5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 00:27:51 +0900

    #144 Creating Room 12

--- socketHandlers/roomCreateHandler.js
+++ socketHandlers/roomCreateHandler.js
@@ -1,8 +1,11 @@
+const serverStore = require("../serverStore");
+
 const roomCreateHandler = (socket) => {
     console.log("roomCreateHandler")
     const socketId = socket.id;
     const userId = socket.user.userId;
 
+    const roomDetails = serverStore.addNewActiveRoom(userId, socketId)
 
     socket.emit("room-create", {
         roomDetails
