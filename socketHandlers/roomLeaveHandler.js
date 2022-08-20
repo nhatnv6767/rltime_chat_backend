b1bfaaa01ac6e66f2d0b95897310e68551009c54
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 13:09:39 +0900

    #153 Creating leave room functionality 7

--- socketHandlers/roomLeaveHandler.js
+++ socketHandlers/roomLeaveHandler.js
@@ -3,6 +3,14 @@ const roomsUpdate = require("./updates/rooms")
 
 const roomLeaveHandler = (socket, data) => {
     const {roomId} = data;
+
+    const activeRoom = serverStore.getActiveRoom(roomId);
+
+    if (activeRoom) {
+        serverStore.leaveActiveRoom(roomId, socket.id)
+
+        roomsUpdate.updateRooms();
+    }
 }
 
 module.exports = roomLeaveHandler
