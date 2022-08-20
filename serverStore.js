812b6caa4bf0f15b57be46faba959f2e18e067e5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-20 13:16:00 +0900

    #153 Creating leave room functionality 9

--- serverStore.js
+++ serverStore.js
@@ -107,6 +107,11 @@ const leaveActiveRoom = (roomId, participantSocketId) => {
         const copyOfActiveRoom = {...activeRoom};
         copyOfActiveRoom.participants = copyOfActiveRoom.participants
             .filter(participant => participant.socketId !== participantSocketId)
+
+        activeRooms = activeRooms.filter(room => room.roomId !== roomId)
+        if (copyOfActiveRoom.participants.length > 0) {
+            activeRooms.push(copyOfActiveRoom)
+        }
     }
 }
 
@@ -121,4 +126,5 @@ module.exports = {
     getActiveRooms,
     getActiveRoom,
     joinActiveRoom,
+    leaveActiveRoom,
 };
