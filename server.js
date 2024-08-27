61da4aae62c384bf27d29c5efe46d40b722a7458
bhnone <>
2024-08-28 01:57:13 +0900

    #175 Fixing bug when last user will leave room #

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const socketServer = require('./socketServer');
-const authRoutes = require('./routes/authRoutes');
-const friendInvitationRoutes = require('./routes/friendInvitationRoutes');
+const socketServer = require('./socketServer')
+const authRoutes = require('./routes/authRoutes')
+const friendInvitationRoutes = require('./routes/friendInvitationRoutes')
 
-const PORT = process.env.PORT || process.env.API_PORT;
+const PORT = process.env.PORT || process.env.API_PORT
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.use('/api/auth', authRoutes);
-app.use("/api/friend-invitation", friendInvitationRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/friend-invitation', friendInvitationRoutes)
 
-const server = http.createServer(app);
-socketServer.registerSocketServer(server);
+const server = http.createServer(app)
+socketServer.registerSocketServer(server)
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    server.listen(PORT, () => {
-        console.log(`Server is listening on ${PORT}`);
-    });
+  server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+  })
 }).catch(err => {
-    console.log('Database connection failed. Server not started.');
-    console.error(err);
-});
+  console.log('Database connection failed. Server not started.')
+  console.log('Database connection failed. Server not started.')
+  console.error(err)
+})
