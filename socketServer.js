8021a1d790b843a64cc086d5dcf1eb7eeacdf0db
bhnone <>
2024-08-28 01:55:21 +0900

    #175 Fixing bug when last user will leave room 2

--- socketServer.js
+++ socketServer.js
@@ -1,82 +1,86 @@
-const authSocket = require('./middleware/authSocket');
-const newConnectionHandler = require("./socketHandlers/newConnectionHandler");
-const disconnectHandler = require("./socketHandlers/disconnectHandler");
-const serverStore = require("./serverStore");
-const directMessageHandler = require("./socketHandlers/directMessageHandler")
-const directChatHistoryHandler = require("./socketHandlers/directChatHistoryHandler")
-const roomCreateHandler = require("./socketHandlers/roomCreateHandler")
-const roomJoinHandler = require("./socketHandlers/roomJoinHandler")
-const roomLeaveHandler = require("./socketHandlers/roomLeaveHandler")
-const roomInitializeConnectionHandler = require("./socketHandlers/roomInitializeConnectionHandler")
-const roomSignalingDataHandler = require("./socketHandlers/roomSignalingDataHandler")
+const authSocket = require('./middleware/authSocket')
+const newConnectionHandler = require('./socketHandlers/newConnectionHandler')
+const disconnectHandler = require('./socketHandlers/disconnectHandler')
+const serverStore = require('./serverStore')
+const directMessageHandler = require('./socketHandlers/directMessageHandler')
+const directChatHistoryHandler = require(
+  './socketHandlers/directChatHistoryHandler')
+const roomCreateHandler = require('./socketHandlers/roomCreateHandler')
+const roomJoinHandler = require('./socketHandlers/roomJoinHandler')
+const roomLeaveHandler = require('./socketHandlers/roomLeaveHandler')
+const roomInitializeConnectionHandler = require(
+  './socketHandlers/roomInitializeConnectionHandler')
+const roomSignalingDataHandler = require(
+  './socketHandlers/roomSignalingDataHandler')
 
 const registerSocketServer = (server) => {
-    const io = require('socket.io')(server, {
-        cors: {
-            origin: '*',
-            methods: ['GET', 'POST'],
-        },
-    });
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  })
 
-    serverStore.setSocketServerInstance(io)
+  serverStore.setSocketServerInstance(io)
 
-    io.use((socket, next) => {
-        authSocket(socket, next);
-    });
+  io.use((socket, next) => {
+    authSocket(socket, next)
+  })
 
-    const emitOnlineUsers = () => {
-        const onlineUsers = serverStore.getOnlineUsers();
-        io.emit(
-            "online-users", {onlineUsers}
-        )
-    }
+  const emitOnlineUsers = () => {
+    const onlineUsers = serverStore.getOnlineUsers()
+    io.emit(
+      'online-users', { onlineUsers },
+    )
+  }
 
-    io.on('connection', async (socket) => {
-        console.log('user connected');
-        console.log(socket.id);
-        await newConnectionHandler(socket, io);
+  io.on('connection', async (socket) => {
+    console.log('user connected')
+    console.log('user connected')
+    console.log(socket.id)
+    await newConnectionHandler(socket, io)
 
-        emitOnlineUsers()
+    emitOnlineUsers()
 
-        socket.on("direct-message", (data) => {
-            directMessageHandler(socket, data);
-        })
+    socket.on('direct-message', (data) => {
+      directMessageHandler(socket, data)
+    })
 
-        socket.on("direct-chat-history", (data) => {
-            directChatHistoryHandler(socket, data);
-        })
+    socket.on('direct-chat-history', (data) => {
+      directChatHistoryHandler(socket, data)
+    })
 
-        socket.on("room-create", () => {
-            roomCreateHandler(socket)
-        })
+    socket.on('room-create', () => {
+      roomCreateHandler(socket)
+    })
 
-        socket.on("room-join", (data) => {
-            roomJoinHandler(socket, data)
-        })
+    socket.on('room-join', (data) => {
+      roomJoinHandler(socket, data)
+    })
 
-        socket.on("room-leave", data => {
-            roomLeaveHandler(socket, data)
-        })
+    socket.on('room-leave', data => {
+      roomLeaveHandler(socket, data)
+    })
 
-        socket.on("conn-init", (data) => {
-            roomInitializeConnectionHandler(socket, data)
-        })
+    socket.on('conn-init', (data) => {
+      roomInitializeConnectionHandler(socket, data)
+    })
 
-        socket.on("conn-signal", data => {
-            roomSignalingDataHandler(socket, data)
-        })
+    socket.on('conn-signal', data => {
+      roomSignalingDataHandler(socket, data)
+    })
 
-        socket.on("disconnect", () => {
-            disconnectHandler(socket)
-        })
+    socket.on('disconnect', () => {
+      disconnectHandler(socket)
+    })
 
-    });
+  })
 
-    setInterval(() => {
-        emitOnlineUsers()
-    }, 8000)
-};
+  setInterval(() => {
+    emitOnlineUsers()
+  }, 8000)
+}
 
 module.exports = {
-    registerSocketServer,
-};
+  registerSocketServer,
+}
